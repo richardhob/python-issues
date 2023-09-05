5762690c5a32b60fd206c7c1ff36c7e8d995ae5c
Ricky <richardhob@gmail.com>
2023-09-05 07:21:28 -0700

    Add test for copy and sparse copy (fails)

--- 54225/test_sparse.py
+++ 54225/test_sparse.py
@@ -1,5 +1,6 @@
 
 import os
+import shutil
 
 import pytest
 
@@ -21,6 +22,18 @@ def test_sparse(sparse):
     assert os.stat(sparse).st_size == 1024
     assert os.stat(sparse).st_blocks*512 == 0
 
+def test_copy_sparse(tmp_path, sparse):
+    copy = tmp_path / "copy.img"
+    shutil.copyfile(sparse, copy)
+    assert os.stat(copy).st_size == 1024
+    assert os.stat(copy).st_blocks * 512 == 0
+
 def test_normal(normal):
     assert os.stat(normal).st_size == 1024
-    assert os.stat(normal).st_blocks*512 == 8*512
+    assert os.stat(normal).st_blocks*512 == 8 * 512
+
+def test_copy_normal(tmp_path, normal):
+    copy = tmp_path / "normal_copy.img"
+    shutil.copyfile(normal, copy)
+    assert os.stat(copy).st_size == 1024
+    assert os.stat(copy).st_blocks * 512 == 8 * 512
