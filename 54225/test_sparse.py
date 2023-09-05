f98cd787ba35e070e78b5d9fdf7db037a42c3572
Ricky Hob <richardhob@gmail.com>
2023-09-04 21:31:31 -0700

    Write test to verify normal files as well

--- 54225/test_sparse.py
+++ 54225/test_sparse.py
@@ -10,6 +10,17 @@ def sparse(tmp_path):
         out.truncate(1024)
     return path
 
+@pytest.fixture
+def normal(tmp_path):
+    path = tmp_path / "normal.img"
+    with open(path, "ab") as out:
+        out.write(b'\0'*1024)
+    return path
+
 def test_sparse(sparse):
     assert os.stat(sparse).st_size == 1024
     assert os.stat(sparse).st_blocks*512 == 0
+
+def test_normal(normal):
+    assert os.stat(normal).st_size == 1024
+    assert os.stat(normal).st_blocks*512 == 8*512
